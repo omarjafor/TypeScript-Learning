b77c95b909bb0ec6070707a15b55721dbb9cb21b
omarjafor <omarjaforchy@gmail.com>
2024-11-19 15:06:47 +0600

    learning ts node

---
+++ module/src/1.6to1.12.ts
@@ -0,0 +1,18 @@
+// learning function
+// normal function or arrow function 
+
+function addSum(num1: number, num2: number) : number {
+    return num1 + num2;
+}
+addSum(2, 3);
+const number = 2;
+const number2 = 3;
+console.log(addSum(number, number2));
+
+
+const age2 = 20;
+if (age2 >= 18) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a minor");
+}
